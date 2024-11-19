1d1208f9a7d9719723f88777844316d800e52395
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-11-19 14:13:03 +0000

    Routingの設定

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,4 +7,43 @@ use Illuminate\Http\Request;
 class CategoriesController extends Controller
 {
     //
+    public function add()
+    {
+        return view('category.create');
+    }
+
+    public function index()
+    {
+        return view('category.create');
+    }
+
+    public function create()
+    {
+        return redirect('category.create');
+    }
+
+    public function edit()
+    {
+        return view('category.create');
+    }
+
+    public function update()
+    {
+        return redirect('category.create');
+    }
+
+    public function new()
+    {
+        return redirect('category.create');
+    }
+
+    public function show()
+    {
+        return redirect('category.create');
+    }
+
+    public function destroy()
+    {
+        return redirect('category.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('stockmate/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\CategoriesController;
+Route::controller(CategoriesContr::class)->group(function() {
+    Route::get('stockmate', 'add');
+    Route::get('stockmate', 'index');
+    Route::get('stockmate', 'create');
+    Route::get('stockmate', 'edit');
+    Route::get('stockmate', 'update');
+    Route::get('stockmate', 'new');
+    Route::get('stockmate', 'show');
+    Route::get('stockmate', 'new');
+    Route::get('stockmate', 'destroy');
+});
+
+
